7791545cbf3469f466b1d93a0dc6a6096b231d01
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-02-05 21:40:38 -0300

    Add Navbar

---
+++ src/core/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss';
+
+
+const ButtonIcon = () => (
+      <button className="btn-text btn-content ">
+        Começar
+      </button>  
+);
+
+export default ButtonIcon;
