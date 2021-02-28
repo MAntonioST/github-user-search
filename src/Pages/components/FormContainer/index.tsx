beb6fa275cdab44f67b6887da9cb19adcf7c210e
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-02-28 10:24:11 -0300

    Issues#1 - Layout Before

--- src/Pages/components/FormContainer/index.tsx
+++ src/Pages/components/FormContainer/index.tsx
@@ -9,70 +9,69 @@ import ButtonIcon from '../../../core/components/ButtonIcon';
 
 
 type UserNameState = {
-    login:string;
-    
-  }
+    login: string;
 
- type FormEvent = React.ChangeEvent<HTMLInputElement >;
+}
 
- const FormContainer = () => {
-   
+type FormEvent = React.ChangeEvent<HTMLInputElement>;
+
+const FormContainer = () => {
+
     const [userName, setUserName] = useState<UserNameState>({
-        login:'',
-      });
+        login: '',
+    });
 
-    const [ user, setUser] = useState<User>();
+    const [user, setUser] = useState<User>();
     const [isLoading, setIsLoading] = useState(false);
-     
-      const handleOnChange = (event: FormEvent) => {
+
+    const handleOnChange = (event: FormEvent) => {
         const login = event.target.name;
         const value = event.target.value;
         setUserName(data => ({ ...data, [login]: value }));
-        
-    }
-  
-   
-   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
 
-    const payload = {
-     login:userName.login
     }
-    setIsLoading(true);
-    makeRequest({ url: `/users/${payload.login}`})
-       .then(response => setUser(response.data))
-       .finally(() => setIsLoading(false));
-    
-       setUserName({login:''});
- }
-     
+
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const payload = {
+            login: userName.login
+        }
+        setIsLoading(true);
+        makeRequest({ url: `/users/${payload.login}` })
+            .then(response => setUser(response.data))
+            .finally(() => setIsLoading(false));
+
+        setUserName({ login: '' });
+    }
+
     return (
         <>
-           <form onSubmit={handleSubmit}>
-            <div className="form-container">
-                <div className="form-contents-title">
-                    Encontre um perfil Github
+            <form onSubmit={handleSubmit}>
+                <div className="form-container">
+                    <div className="form-contents-title">
+                        Encontre um perfil Github
             </div>
-                <div className="form-inputs">
-                    <input
-                        value={userName.login}
-                        name="login"
-                        type="text"
-                        onChange={handleOnChange}
-                        className="form-control form-inputs-text"
-                        placeholder="Usuário Github"
-                    />
-                </div>
+                    <div className="form-inputs">
+                        <input
+                            value={userName.login}
+                            name="login"
+                            type="text"
+                            onChange={handleOnChange}
+                            className="form-control form-inputs-text"
+                            placeholder="Usuário Github"
+                        />
+                    </div>
                     <div className="btn-icon-form">
                         <ButtonIcon text="Encontrar" />
                     </div>
-              
-            </div>
+
+                </div>
             </form>
-            
-            <div className="form-container-users">
-                <SearchUser  user={user} key={user?.id} isLoading={isLoading}/>
-            </div>
+                <div className="form-container-users">           
+                {user? <SearchUser user={user} key={user?.id} isLoading={isLoading} /> : ""}
+                </div>
         </>
 
     );
